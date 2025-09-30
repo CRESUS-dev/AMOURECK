39612857366ad743556c1c15e7560221e9e9a5d0
CRESUS <paparoger70@gmail.com>
2025-09-30 00:36:31 +0000

    after staticfiles settings and before media files settings

--- app/AMOURECK/settings.py
+++ app/AMOURECK/settings.py
@@ -21,8 +21,8 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 DEBUG = (os.environ.get('DEBUG') == "True")
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', "127.0.0.1").split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv("DJANGO_CSRF_TRUSTED_ORIGINS", "https://127.0.0.1").split(",")
-#
+CSRF_TRUSTED_ORIGINS =  ["http://localhost:1337"]
+
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('SQL_ENGINE'),
