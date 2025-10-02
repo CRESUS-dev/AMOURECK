5e33324ca451ae0fdec99fd4752604ad0b495ab7
= <cresus@CR35U5.localdomain>
2025-10-02 14:44:22 +0000

    collect staticfiles

--- AMOURECK/settings.py
+++ AMOURECK/settings.py
@@ -157,8 +157,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # glabals files
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # collectstatic en prod
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Media files (users uploads)
